3323b7b6265152b505579337b4a053018e734ba8
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-08-26 02:25:14 +0530

    First Commit Basic Movement Setup Done

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+
+public class TopDownMovement : MonoBehaviour
+{
+
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float controllerDeadZone= 0.1f;
+
+    private CharacterController controller;
+
+    private Vector2 movement;
+    private Vector3 playerVelocity;
+
+    private PlayerControls playerControls;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerControls = new PlayerControls();
+        playerInput = GetComponent<PlayerInput>();  
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //    
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+    void HandleInput()
+    {
+        movement = playerControls.Controls.Movement.ReadValue<Vector2>();
+    }
+    void HandleMovement()
+    {
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
